b4ce21d29d2d93bdc612619bb042ca94f27db7e6
quim-romero <quimromero22@gmail.com>
2024-12-27 10:45:00 +0100

    Create base view files for routing (Home, About, Contact, 404)

---
+++ src/pages/Contact.jsx
@@ -0,0 +1,3 @@
+export default function Contact() {
+  return <h1 className="text-3xl font-bold text-brand">Contact Page</h1>;
+}
---
+++ src/pages/NotFound.jsx
@@ -0,0 +1,3 @@
+export default function NotFound() {
+  return <h1 className="text-3xl text-red-500">404 – Page Not Found</h1>;
+}
