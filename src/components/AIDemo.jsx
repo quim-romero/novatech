59da720ab1e2f6e1bcbc3856164a66db5745d37a
quim-romero <quimromero22@gmail.com>
2025-01-04 11:00:00 +0100

    Create placeholder components for TechShowcase demos

---
+++ src/components/AIDemo.jsx
@@ -0,0 +1,3 @@
+export default function AIDemo() {
+  return <div className="h-64 bg-gray-200 rounded-lg flex items-center justify-center">[Chart Placeholder]</div>;
+}
---
+++ src/components/SecurityAlerts.jsx
@@ -0,0 +1,3 @@
+export default function SecurityAlerts() {
+  return <div className="h-64 bg-gray-200 rounded-lg flex items-center justify-center">[Chart Placeholder]</div>;
+}
